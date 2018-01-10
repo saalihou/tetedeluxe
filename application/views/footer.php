4b7ecd409bccf37c138bfafc3c97f9f86840d38c
Saalihou Ndiaye <saalihou@gmail.com>
2018-01-10 10:35:47 +0000

    feat: Implement home page

---
+++ application/views/footer.php
@@ -0,0 +1,5 @@
+  <div class="text-center">
+    <h5>Copyright TDL 2018</h5>
+  </div>
+</body>
+</html>
---
+++ application/views/home.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<?php
+	$this->load->view('header');
+?>
+<div id="container">
+	<div class="jumbotron">
+		<h1>Bienvenue au salon TeteDeLuxe!</h1>
+		<p cass="lead">
+			Parcourez notre catalogue, faite votre choix et réservez votre rendez-vous!
+		</p>
+	</div>
+</div>
+<?php
+	$this->load->view('footer');
+?>
